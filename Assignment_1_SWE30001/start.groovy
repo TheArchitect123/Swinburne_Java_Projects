3787242f0a0100a2a82c5f65874e1a2c50da52d8
Dan Gerchcovich <dan.developer789@gmail.com>
2020-09-08 02:49:06 -0700

    Assignment 1 Initial Commit

---
+++ Assignment_1_SWE30001/start.groovy
@@ -0,0 +1,5 @@
+@Grab("com.danGerchcovich:spring-boot-starter-actuator:0.5.0.BUILD-SNAPSHOT")
+@Grab("com.danGerchcovich:spring-boot-starter-web:0.5.0.BUILD-SNAPSHOT")
+@ComponentScan("com.danGerchcovich.sample.service")
+class Start {
+}
